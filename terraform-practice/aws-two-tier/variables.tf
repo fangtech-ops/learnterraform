7378209a462daab78ea4dc766ee1031ba2f2b6a7
Charles Lee <clee6846@gmail.com>
2024-03-25 22:20:36 -0700

    3-25-24-1

--- terraform-practice/aws-two-tier/variables.tf
+++ terraform-practice/aws-two-tier/variables.tf
@@ -9,12 +9,7 @@ DESCRIPTION
     default = "/Users/charles.lee/.ssh/terraform.pub"
 
 }
-variable "aws_access_key" {
-    default = ""
-}
-variable "aws_secret_key" {
-    default = ""
-}
+
 variable "aws_key_path" {
     default = "/Users/charles.lee/.ssh/terraform"
 }
